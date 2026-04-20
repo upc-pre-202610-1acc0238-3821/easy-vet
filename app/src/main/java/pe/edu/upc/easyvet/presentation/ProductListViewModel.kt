6ed00f57f56d7943fd5be57d05f782b29c7fe2a5
jmayta1984 <jmayta@pucp.edu.pe>
2026-04-20 14:19:34 -0500

    refactor: update `ProductListViewModel` to use repository interface and implement product fetching
    - Update `ProductListViewModel` to depend on the `ProductRepository` interface instead of the concrete implementation.
    - Implement the `getProducts` function using `viewModelScope` to fetch and update the product list state.

--- app/src/main/java/pe/edu/upc/easyvet/presentation/ProductListViewModel.kt
+++ app/src/main/java/pe/edu/upc/easyvet/presentation/ProductListViewModel.kt
@@ -1,12 +1,24 @@
 package pe.edu.upc.easyvet.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import pe.edu.upc.easyvet.data.ProductRepositoryImpl
+import kotlinx.coroutines.launch
 import pe.edu.upc.easyvet.domain.Product
+import pe.edu.upc.easyvet.domain.ProductRepository
 
-class ProductListViewModel (val productRepository: ProductRepositoryImpl): ViewModel() {
+class ProductListViewModel(
+    private val productRepository: ProductRepository
+) : ViewModel() {
     private var _products = MutableStateFlow<List<Product>>(emptyList())
     val products: StateFlow<List<Product>> get() = _products
+
+    fun getProducts() {
+        viewModelScope.launch {
+           _products.value = productRepository.getProducts()
+        }
+
+    }
+
 }
