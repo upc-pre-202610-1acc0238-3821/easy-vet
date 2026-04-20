aea56176077743b098ecb8d1b4d510b6b8167428
jmayta1984 <jmayta@pucp.edu.pe>
2026-04-20 15:24:23 -0500

    feat: update `MainActivity` to display `ProductList` and initialize `ProductListViewModel`
    - Instantiate `ProductListViewModel` using `ProductRepository` from `RepositoryModule`.

--- app/src/main/java/pe/edu/upc/easyvet/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/easyvet/MainActivity.kt
@@ -8,18 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import pe.edu.upc.easyvet.di.RepositoryModule.provideProductRepository
 import pe.edu.upc.easyvet.presentation.Login
+import pe.edu.upc.easyvet.presentation.ProductList
+import pe.edu.upc.easyvet.presentation.ProductListViewModel
 import pe.edu.upc.easyvet.presentation.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val viewModel = ProductListViewModel(
+            provideProductRepository()
+        )
         setContent {
-            AppTheme  {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Login(Modifier.padding(innerPadding))
-                }
+            AppTheme {
+                ProductList(viewModel)
             }
         }
     }
