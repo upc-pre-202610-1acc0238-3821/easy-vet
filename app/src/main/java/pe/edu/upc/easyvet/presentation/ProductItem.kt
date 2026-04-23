a33eb116258a64954cd765d8e7ad4c36a6b968ed
jmayta1984 <jmayta@pucp.edu.pe>
2026-04-23 11:27:20 -0500

    feat: add product image to `ProductItem` using Coil
    - Implement `AsyncImage` to display the product's image within the `ProductItem` card.
    - Set the image height to 256.dp and configure it to fill the maximum width.

--- app/src/main/java/pe/edu/upc/easyvet/presentation/ProductItem.kt
+++ app/src/main/java/pe/edu/upc/easyvet/presentation/ProductItem.kt
@@ -2,6 +2,7 @@ package pe.edu.upc.easyvet.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import pe.edu.upc.easyvet.domain.Product
 
 @Composable
@@ -21,6 +23,13 @@ fun ProductItem(product: Product) {
     ) {
 
         Column(modifier = Modifier.padding(8.dp)) {
+            AsyncImage(
+                model = product.image,
+                contentDescription = product.name,
+                modifier = Modifier
+                    .height(256.dp)
+                    .fillMaxWidth()
+            )
             Text(product.name, fontWeight = FontWeight.SemiBold)
             Text(product.description, maxLines = 1, overflow = TextOverflow.Ellipsis)
         }
